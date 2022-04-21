bf46fdd74b545277d143132ce2aa7b4efe0505a2
itsAllyx <allyzzaguanzing@gmail.com>
2022-04-21 14:05:16 +0800

    add log-in design

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -1,6 +1,9 @@
-import { Box, Paper, TextField, Typography,AccountCircle } from '@mui/material'
+import { Box, Paper, Button, TextField, Typography, Checkbox, AccountCircle } from '@mui/material'
 import { borderRadius, fontSize, fontWeight, margin } from '@mui/system'
 import React from 'react'
+import Image from "next/image";
+
+
 
 export default function SignIn() {
   return (
@@ -21,17 +24,34 @@ export default function SignIn() {
     alignItems:"center"
    
 }}>
+    <Box sx={{
+        position:"absolute",
+        marginBottom:"490px",
+        marginLeft:"10px"
+    }} >
+        <Image src="/pictures/bsu-logo.png" height={150} width={150} />
+    </Box>
     <Typography sx={{
         fontFamily:"Fraunces",
         fontSize:"30px",
         color:"black",
-        marginLeft:"150px"
+        marginLeft:"150px",
+        marginBottom:"100px"
     }}>
 SCHOLARSHIP INFORMATION
 <br></br> MANAGEMENT SYSTEM AND
 <br></br>ONLINE APPLICATION WEB-BASED<br></br> SYSTEM
 FOR BULACAN STATE<br></br> UNIVERSITY BUSTOS CAMPUS      
     </Typography>
+    <Box sx={{
+        position:"absolute",
+        marginTop:"355px",
+        display:"flex",
+        flexDirection:"row"
+    }} >
+        <Image src="/pictures/scholar.png" height={300} width={800} />
+        <Image src="/pictures/scholar.png" height={300} width={600} />
+    </Box>
 <Paper elevation={3} sx={{
     height:"600px",
     width:"500px",
@@ -45,25 +65,64 @@ FOR BULACAN STATE<br></br> UNIVERSITY BUSTOS CAMPUS
     textAlign:"center",
     
 }}>
+    <Box sx={{
+        width:"100px",
+        height:"100px",
+        backgroundColor:"#D1D1D1",
+        borderRadius:"50%",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        position:"absolute",
+        marginTop:"-580px",
+        marginLeft:"200px",
+        border:"3px solid white"
+    }} >
+        <Image src="/svg/user.svg"  width={40} height={40}/>
+    </Box>
 <Typography sx={{
     fontFamily:"fanwood text",
     fontSize:"25px",
     color:"white",
-    marginTop:"100px"
+    marginTop:"100px",
+    marginBottom:"50px"
     }}>
  LOG IN TO YOUR ACCOUNT
 </Typography>
 
-      
-        <TextField id="input-with-sx" label="Email:" variant="standard" sx={{
-            width:"200px",
-            marginLeft:"200px",
-            
-        }} />
-        <TextField id="input-with-sx" label="Password:" variant="standard" sx={{
-            width:"200px",
-            marginLeft:"200px"
-        }} />
+<Box sx={{
+    display:"flex",
+    flexDirection:"row",
+    marginLeft:"60px"
+}} >
+     <Box sx={{
+         width:"60px",
+         marginTop:"10px"
+     }} >
+        <Image src="/svg/email.svg"  height={40} width={40} />
+     </Box>
+
+<TextField id="filled-basic" label="Email:" type="email" required variant="standard" sx={{
+    width:"300px"
+}} />
+</Box>
+<Box sx={{
+    display:"flex",
+    flexDirection:"row",
+    marginLeft:"60px",
+    marginTop:"10px"
+}} >
+     <Box sx={{
+         width:"60px",
+         marginTop:"10px"
+     }} >
+        <Image src="/svg/password.svg"  height={40} width={40} />
+     </Box>
+
+<TextField id="filled-basic" label="Password:" type="password" required variant="standard" sx={{
+    width:"300px"
+}} />
+</Box>
 <Box sx={{
     height:"50vh",
     width:"50vw",
@@ -75,20 +134,32 @@ FOR BULACAN STATE<br></br> UNIVERSITY BUSTOS CAMPUS
         fontFamily:"fanwood text",
         fontSize:"15px",
         color:"white",
-        marginLeft:"60px"
+        marginLeft:"30px"
     }}>
+        <Checkbox label="Remember me" />
 Remember me
     </Typography>
     <Typography sx={{
         fontFamily:"fanwood text",
         fontSize:"15px",
         color:"white",
-        marginLeft:"160px"
+        marginLeft:"160px",
+        marginTop:"10px"
     }}>
 Forgot Password?
     </Typography>
 </Box>
- 
+<Button variant="contained" sx={{
+    marginBottom:"100px",
+    marginLeft:"180px",
+    height:"100px",
+    width:"150px",
+    backgroundColor:"white",
+    color:"black",
+    fontFamily:"fanwood text",
+    fontWeight:"bold",
+    borderRadius:"20px"
+}} >Log In</Button>
       
 </Paper>
 
